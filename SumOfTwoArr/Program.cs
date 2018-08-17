7df9dd47836c88a57b97f4f467a363878acaa612
Beata-Krasnopolska <beata.krasnopolska@interia.pl>
2018-08-17 17:30:27 +0200

    Add project files.

---
+++ SumOfTwoArr/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumOfTwoArr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3 };
+            int[] b = { 10, 20, 30, 40 };
+            int v = 42;
+
+            var sumNumber = new SumNumbers();
+            var result = sumNumber.SumOfTwo(a, b, v);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SumOfTwoArr/SumNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SumOfTwoArr
+{
+    class SumNumbers
+    {
+          public bool SumOfTwo(int[] a, int[] b, int v)
+        {
+            v = 0;
+
+            bool sumPossible = false;
+                       
+            for (int i = 0; i < a.Length; i++)
+            {
+                
+                for (int j = 0; j < b.Length; j++)
+                {
+                    v = a[i] + b[i];
+                    sumPossible = true;
+                                   
+                }
+            }
+            if(sumPossible == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
